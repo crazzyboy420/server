88a63758e569979ada949df1afde3b9d763c498f
crazzyboy420 <raselbabu34.bd@gmail.com>
2022-12-29 03:30:58 +0600

    server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
-const port = 8000;
+const PORT = 8000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(PORT);
