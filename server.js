3fee51b0e912ce9e851b150e8fe1306fe2406b61
crazzyboy420 <raselbabu34.bd@gmail.com>
2022-12-29 04:27:51 +0600

    up

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
-
 const port = process.env.port || 9000;
 
 server.use(middlewares);
-
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1'
-}));
-server.use(router)
+server.use('',router)
 server.listen(port, () => {
     console.log(`Server is runnint port on ${port}`)
 })
